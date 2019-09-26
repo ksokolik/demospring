d1755a03a0166cebaafd848ebe3a668c162f7e2a
ksokolik <ksokolik@gmail.com>
2019-09-26 14:21:42 +0200

    Simple REST controller

---
+++ src/main/java/com/demospring/demospring/SimpleController.java
@@ -0,0 +1,15 @@
+package com.demospring.demospring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String homePage() {
+        return "Hello World v1";
+    }
+}
